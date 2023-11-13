c656c9ce6c549f9c3d5a5d96a7ef2e143224fefd
Rodrigo Lima <limarodrigoo@outlook.com>
2023-11-13 17:00:50 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Projeto de Geração de Tabela de Empresas da Bolsa de Valores
+
+Bem-vindo ao projeto de geração de tabela de empresas da Bolsa de Valores! Este é um projeto em Python que permite listar e filtrar informações sobre empresas listadas na bolsa de valores. Este guia irá ajudá-lo a instalar e executar o projeto, mesmo se você não tiver experiência técnica avançada.
+
+## Requisitos Prévios
+Antes de começar, certifique-se de ter o seguinte instalado em seu sistema:
+
+- [Python](https://www.python.org/downloads/): Certifique-se de ter o Python instalado em seu sistema. Você pode baixar a versão mais recente [aqui](https://www.python.org/downloads/).
+
+## Instalação
+
+1. **Clone ou Baixe o Projeto:**
+    - Clone o projeto usando o comando:
+      ```
+      git clone https://github.com/seu-usuario/nome-do-projeto.git
+      ```
+    - Ou baixe o projeto como um arquivo ZIP e extraia-o.
+
+2. **Navegue até o Diretório do Projeto:**
+    - Abra o terminal (ou prompt de comando) e navegue até o diretório onde você colocou o projeto.
+
+3. **Crie um Ambiente Virtual (Opcional, mas recomendado):**
+    - Execute o seguinte comando para criar um ambiente virtual (ambiente isolado para o projeto):
+      ```
+      python -m venv venv
+      ```
+
+4. **Ative o Ambiente Virtual:**
+    - No Windows, use o comando:
+      ```
+      venv\Scripts\activate
+      ```
+    - No Linux/Mac, use o comando:
+      ```
+      source venv/bin/activate
+      ```
+
+5. **Instale as Dependências:**
+    - Execute o comando para instalar as dependências do projeto:
+      ```
+      pip install -r requirements.txt
+      ```
+
+## Uso
+
+1. **Execute o Projeto:**
+    - No terminal, execute o seguinte comando:
+      ```
+      python main.py
+      ```
+
+2. **Siga as Instruções:**
+    - O programa irá guiá-lo através das opções disponíveis. Você poderá inserir filtros para a tabela de empresas, como setor, país, etc.
+
+3. **Visualize a Tabela:**
+    - Após a execução, o programa gerará uma tabela com as informações filtradas das empresas da bolsa de valores.
+
+4. **Saia do Ambiente Virtual (Opcional):**
+    - Quando terminar, você pode sair do ambiente virtual usando o comando:
+      ```
+      deactivate
+      ```
+
+## Contribuições
+Se você encontrar problemas ou tiver sugestões para melhorar o projeto, sinta-se à vontade para abrir uma [issue](https://github.com/seu-usuario/nome-do-projeto/issues) ou enviar um [pull request](https://github.com/seu-usuario/nome-do-projeto/pulls).
+
+Divirta-se explorando as informações das empresas da bolsa de valores!
---
+++ src/main.py
@@ -0,0 +1,92 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+url = "https://www.fundamentus.com.br/resultado.php"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
+
+
+def limpar_e_converter(valor):
+    if "%" in valor:
+        multiplicador = -1 if "-" in valor else 1
+        return (
+            multiplicador * float(valor.strip("%").replace(".", "").replace(",", "."))
+        ) / 100
+    elif "." in valor:
+        return float(valor.replace(".", "").replace(",", "."))
+    else:
+        return float(valor.replace(",", "."))
+
+
+# Encontrar a tabela pelo ID "resultado"
+table = soup.find("table", id="resultado")
+
+if table:
+    colunas_desejadas = [
+        "Papel",
+        "Cotação",
+        "P/L",
+        "ROE",
+        "Dív.Brut/ Patrim.",
+        "Cresc. Rec.5a",
+        "Liq.2meses",
+        "Patrim. Líq",
+        "ROIC",
+        "P/VP",
+        "Div.Yield",
+        "Mrg. Líq.",
+    ]
+    # Extrair dados da tabela usando pandas
+    filtro_dict = {
+        "P/L": ("<", 15),
+        "ROE": (">=", 0.15),
+        "Dív.Brut/ Patrim.": ("<=", 1),
+        "Cresc. Rec.5a": (">=", 0.1),
+        "Liq.2meses": (">=", 2000000),
+        "Patrim. Líq": (">", 0),
+        "ROIC": (">=", 0.15),
+        "Mrg. Líq.": (">=", 0.1),
+    }
+    data = []
+    for row in table.find_all("tr"):
+        cols = row.find_all(["td", "th"])
+        data.append([col.text.strip() for col in cols])
+
+    # Criar um DataFrame pandas
+    df = pd.DataFrame(data[1:], columns=data[0])
+
+    allColumns = list()
+    for column in df.columns:
+        allColumns.append(column)
+
+    colunas_indesejadas = list(set(colunas_desejadas) ^ set(allColumns))
+
+    for coluna in colunas_indesejadas:
+        df.drop(coluna, axis=1, inplace=True)
+
+    for i, coluna in enumerate(colunas_desejadas):
+        if i != 0:
+            df[coluna] = df[coluna].apply(limpar_e_converter)
+    # Salvar DataFrame em um arquivo Excel
+
+    for coluna, (operacao, valor) in filtro_dict.items():
+        if operacao == ">":
+            df = df[df[coluna] > valor]
+        elif operacao == "<":
+            df = df[df[coluna] < valor]
+        elif operacao == "==":
+            df = df[df[coluna] == valor]
+        elif operacao == ">=":
+            df = df[df[coluna] >= valor]
+        elif operacao == "<=":
+            df = df[df[coluna] <= valor]
+
+    df.to_excel("dados_tabela.xlsx", index=False)
+
+    print("Dados da tabela salvos em dados_tabela.xlsx")
+else:
+    print("Tabela 'resultado' não encontrada.")
